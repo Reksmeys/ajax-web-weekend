06a959bebd61d0d093f4eccf15ff192db16bab30
raksmeys <raksmeymom111@gmail.com>
2019-08-18 11:03:31 +0700

    view article by category and view detail article

--- js/category.js
+++ js/category.js
@@ -2,6 +2,11 @@ $(function(){
     $('#category').change(function(){
         fetchArticleByCategoryId($(this).val())
     })
+    $(document).on('click', '.view-detail', function(e){
+        // console.log($(this).find('.hide').text())
+        //transfer id of article to view-detail page
+        window.location.href = `article-detail.html?id=${$(this).find('.hide').text()}`
+    })
 })
 function fetchArticleByCategoryId(id){
     $.ajax({
@@ -21,13 +26,15 @@ function loadCard(article, msg){
     var content = ""
     for (a of article){
         content +=`
-        <div class="col-md-6 col-sm-6 col-lg-6">
+        <div class="col-md-6 col-sm-6 col-lg-6 view-detail">
                 <div class="card">
                     <img class="card-img-top" src=${a.IMAGE} alt="Card image cap">
                     <div class="card-body">
+                        <p style="display: none" class="hide">${a.ID}</p>
                         <h4 class="card-title"> <a>${a.TITLE}</a></h4>
                         <p class="card-text">${a.DESCRIPTION}</p>
-                        <a href="#" class="btn btn-primary">Button</a>
+                        <button  class="btn btn-primary" >Button</button>
+                        <a href="article-detail.html" >link</a>
                     </div>
                 </div>
         </div>
@@ -35,5 +42,4 @@ function loadCard(article, msg){
     }
     $('.row').html(content)
     toastr.success(`${msg}`)
-
 }
